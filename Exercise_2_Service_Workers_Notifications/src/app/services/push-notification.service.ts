191c1b34a6749f7d5c359ef302d3d708894286f1
yapeaud <yapoabed@gmail.com>
2023-01-27 17:46:29 +0000

    w5d2G

---
+++ Exercise_2_Service_Workers_Notifications/src/app/services/push-notification.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { firebase } from 'firebase';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushNotificationService {
+
+  constructor() { }
+}
---
+++ Exercise_2_Service_Workers_Notifications/src/firebase-messaging.js
@@ -0,0 +1,12 @@
+firebase.initializeApp(
+  {
+    apiKey: "AIzaSyBxKTGPzCo_c9IOcGyiLmPjZlp_iOAxRns",
+    authDomain: "di-bootcamp-week5-day2-xp-gold.firebaseapp.com",
+    projectId: "di-bootcamp-week5-day2-xp-gold",
+    storageBucket: "di-bootcamp-week5-day2-xp-gold.appspot.com",
+    messagingSenderId: "627211335595",
+    appId: "1:627211335595:web:e0c1ad1d2c60597ccb84ee",
+    measurementId: "G-2LB7YH6QD7"
+  }
+)
+const messaging = firebase.messaging();
